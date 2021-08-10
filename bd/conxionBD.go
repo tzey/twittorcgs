569b8192446d5e76d7df9fa02de0dc7fed831e45
Chema Galvez <chemagalvez@gmail.com>
2021-08-10 16:12:16 +0200

    connectionBD

---
+++ bd/conxionBD.go
@@ -0,0 +1,36 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = conectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://root:Chuletones33@cluster0.lhwdu.mongodb.net/test")
+
+// Funcion que me permite conectar a la BD
+func conectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexion exitosa")
+	return client
+}
+
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/tzey/twittorcgs/bd"
+	"github.com/tzey/twittorcgs/handlers"
+)
+
+func main() {
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+	}
+	handlers.Manejadores()
 }
